89b99f4413498c7b23104a9e3d9e9a278879cb86
Gaurav Chaudhari <chaudharigaurav177@gmail.com>
2025-09-26 13:36:11 +0530

    Added `W4_L4_LinkedListUsingJCF` to demonstrate LinkedList and ArrayList operations using Java Collections Framework

---
+++ src/W4_L4_LinkedListUsingJCF/Main.java
@@ -0,0 +1,39 @@
+package W4_L4_LinkedListUsingJCF;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Jay");
+        list.add("Gaurav");
+        list.addFirst("Hello");
+        list.addLast("World");
+        System.out.println(list);
+        list.removeFirst();
+        list.removeLast();
+        System.out.println(list.peek());
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+
+        LinkedList<String> linkedList = new LinkedList<>(arrayList);
+        linkedList.remove("2");
+        System.out.println(linkedList);
+
+
+        for (String s : list) {
+            System.out.print(s + " -> ");
+        }
+
+        System.out.println();
+
+        Iterator<String> iterator = arrayList.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " -> ");
+        }
+    }
+}
